61778b81128869ce786dce338f8d2b28e82465ab
OPP Developer <developer.opp@gmail.com>
2020-06-16 15:34:29 +0400

    Add Student Mark Dialog Modified

--- src/components/AddStudentMarkDialog.vue
+++ src/components/AddStudentMarkDialog.vue
@@ -1,33 +1,36 @@
 <template>
   <q-dialog v-model="isDialogOpen">
     <q-card>
-      <q-card-section class="text-weight-bold">{{ title }}</q-card-section>
-      <q-card-section>
-        <div class="row q-my-sm">
-          <q-list style="width: 100%">
-            <q-item v-for="(distribution, i) in exam.marksDistribution" :key="i">
-              <q-item-section>{{ distribution.text }}</q-item-section>
-              <q-item-section>
-                <div class="row items-center">
-                  <q-input style="width: 100px" dense filled label="درجة الطالب"></q-input>
-                  <strong class="q-ml-sm">/ {{ distribution.marks }}</strong>
-                </div>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </div>
-      </q-card-section>
-      <q-card-actions>
-        <q-space></q-space>
-        <q-btn dense flat color="primary" @click="$emit('closeDialog', false)">إلغاء</q-btn>
-        <q-btn
-          dense
-          flat
-          color="primary"
-          :loading="GET_LOADING"
-          @click="$emit('saveStudentMarks')"
-        >حفظ</q-btn>
-      </q-card-actions>
+      <q-form @submit.prevent="saveStudentMarks()">
+        <q-card-section class="text-weight-bold">{{ title }}</q-card-section>
+        <q-card-section>
+          <div class="row q-my-sm">
+            <q-list style="width: 100%">
+              <q-item v-for="(distribution, i) in exam.marksDistribution" :key="i">
+                <q-item-section>{{ distribution.text }}</q-item-section>
+                <q-item-section>
+                  <div class="row items-center">
+                    <q-input
+                      v-model="input[i]"
+                      style="width: 100px; margin-top: 21px"
+                      dense
+                      filled
+                      label="درجة الطالب"
+                      :rules="[val => val && val > 0 || 'حقل مطلوب']"
+                    ></q-input>
+                    <strong class="q-ml-sm">/ {{ distribution.marks }}</strong>
+                  </div>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </q-card-section>
+        <q-card-actions>
+          <q-space></q-space>
+          <q-btn dense flat color="primary" @click="$emit('closeDialog', false)">إلغاء</q-btn>
+          <q-btn dense flat color="primary" type="submit" :loading="GET_LOADING">حفظ</q-btn>
+        </q-card-actions>
+      </q-form>
     </q-card>
   </q-dialog>
 </template>
@@ -53,12 +56,30 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      input: [],
+      studentMarks: []
+    };
   },
   computed: {
     ...mapGetters({
       GET_LOADING: GETTERS.UI.GET_LOADING
     })
+  },
+  methods: {
+    saveStudentMarks() {
+      this.exam.marksDistribution.forEach((distribution, i) => {
+        this.studentMarks.push({
+          key: distribution.text,
+          value: this.input[i]
+        });
+      });
+
+      console.log("Student Marks", {
+        examType: this.exam.id,
+        marks: this.studentMarks
+      });
+    }
   }
 };
 </script>
